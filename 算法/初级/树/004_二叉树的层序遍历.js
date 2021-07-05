7805550fe8cc04c5a0b7a0f50f4df8721f6cce26
hjc1992 <358984348@qq.com>
2021-07-05 18:52:14 +0800

    for 算法题目

--- 算法/初级/树/004_二叉树的层序遍历.js
+++ 算法/初级/树/004_二叉树的层序遍历.js
@@ -8,7 +8,35 @@ function TreeNode(val, left, right) {
     this.left = (left===undefined ? null : left)
     this.right = (right===undefined ? null : right)
 }
+let left2 = new TreeNode('15', null, null);
+let right2 = new TreeNode('7', null, null);
+let left1 = new TreeNode('9',null, null);
+let right1 = new TreeNode('20',left2, right2);
+let root = new TreeNode('3',left1, right1);
 
-function f1(root) {
-
+/**
+ * 迭代
+ * @param root
+ */
+function f2(root) {
+    if(root == null){
+        return ;
+    }
+    let queue = [];
+    queue.push(root);
+    while(queue.length){
+        let newItems = [];
+        while (queue.length){
+            let item = queue.shift();
+            console.log(item.val);
+            if(item.left){
+                newItems.push(item.left);
+            }
+            if(item.right){
+                newItems.push(item.right);
+            }
+        }
+        queue = newItems;
+    }
 }
+f2(root);
--- 算法/初级/树/005_将有序数组转换为二叉搜索树.js
+++ 算法/初级/树/005_将有序数组转换为二叉搜索树.js
@@ -12,7 +12,53 @@ function TreeNode(val, left, right) {
     this.right = (right===undefined ? null : right)
 }
 
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function f1(nums) {
-
+/**
+ * 递归
+ * @param nums
+ * @returns {TreeNode|boolean}
+ */
+function f1(nums){
+    if(nums == null || !nums.length){
+        return null;
+    }
+    let middleIndex = parseInt(nums.length / 2);
+    let middleItem = nums[middleIndex];
+    let leftNums = nums.slice(0, middleIndex);
+    let rightNums = nums.slice(middleIndex + 1);
+    let node = new TreeNode(middleItem, f1(leftNums), f1(rightNums));
+    return node;
 }
+
+let root = f1(arr);
+console.log(root);
+
+/**
+ * 按层级排
+ * @param nums
+ * @returns {TreeNode}
+ */
+// function f1(nums) {
+//     let queue = [];
+//     let root = new TreeNode();
+//     queue.push(root);
+//     while (nums.length){
+//         let num = nums.shift();
+//         let currentNode = queue.shift();
+//         currentNode.val = num;
+//         if(nums.length > 1){
+//             let leftNode = new TreeNode();
+//             currentNode.left = leftNode;
+//             queue.push(leftNode);
+//             if(nums.length > 2){
+//                 let rightNode = new TreeNode();
+//                 currentNode.right = rightNode;
+//                 queue.push(rightNode);
+//             }
+//         }
+//     }
+//     return root;
+// }
+//
+// console.log(JSON.stringify(f1(arr)));
